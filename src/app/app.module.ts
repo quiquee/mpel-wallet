2fcfd47d48cc4be1dd982e06182257488631ac59
PrisM <thibault.fouache@gmail.com>
2017-11-28 23:07:50 +0100

    Cleanup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {ErrorHandler, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 import {MyApp} from './app.component';
-import {AboutPage} from '../pages/about/about';
 import {ContactPage} from '../pages/contact/contact';
 import {HomePage} from '../pages/home/home';
 import {TabsPage} from '../pages/tabs/tabs';
@@ -12,7 +11,6 @@ import {SplashScreen} from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
     ContactPage,
     HomePage,
     TabsPage
@@ -24,7 +22,6 @@ import {SplashScreen} from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
     ContactPage,
     HomePage,
     TabsPage
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {AboutPage} from '../about/about';
 import {ContactPage} from '../contact/contact';
 import {HomePage} from '../home/home';
 
@@ -8,8 +7,7 @@ import {HomePage} from '../home/home';
 })
 export class TabsPage {
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab2Root = ContactPage;
 
   constructor() {
   }
